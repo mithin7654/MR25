a94c7f2b6af5ad729b166471f10d09d48bba8d99
mithin7654 <mithingaddam99@gmail.com>
2023-08-07 04:27:15 -0400

    loved value

---
+++ src/rproject/p.java
@@ -0,0 +1,7 @@
+package rproject;
+
+public class p {
+public static void main(String[] args) {
+	System.out.println("MR");
+}
+}
---
+++ src/rproject/twofive.java
@@ -0,0 +1,7 @@
+package rproject;
+
+public class twofive {
+ public static void main(String[] args) {
+System.out.println("vachamu");	
+}
+}
